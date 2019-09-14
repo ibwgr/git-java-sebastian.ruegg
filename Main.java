36870c28e730441993e4831ecfbd19046d8988c8
Rüegg Sebastian <Sebastian.Rueegg@swisscom.com>
2019-09-14 14:03:38 +0200

    Uebungsfile mit Fehlern

---
+++ Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String[] monatsNamen = {"Jan", "Feb", "Mar"};
+        int length = monatsNamen.length;
+
+        for (String monatsName : monatsNamen){
+            System.out.println(monatsName)
+        }
+
+
+        for (int i = 0; i < length; i++) {
+            System.out.println(monatsNamen[x]);
+        
+
+    }
+}
