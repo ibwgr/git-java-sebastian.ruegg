9827fcae91453061afeb9756138afc9bee01f560
s3b1R <sebastian.rueegg@bluewin.ch>
2019-09-25 19:18:34 +0200

    3_24 gelöst

---
+++ src/Aufgabe3_24.java
@@ -0,0 +1,13 @@
+public class Aufgabe3_24 {
+    public static void main(String[] args) {
+        int zeilen;
+        zeilen = IOTools.readInteger("Anzahl Zeilen: ");
+        for (int i = 1; i <= zeilen; i++){
+            for (int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
