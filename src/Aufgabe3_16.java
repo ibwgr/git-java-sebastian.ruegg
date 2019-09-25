1b2d665fc6d3b1df9c4ae0f7b991edde86117da4
s3b1R <sebastian.rueegg@bluewin.ch>
2019-09-25 07:16:44 +0200

    3_16 gelöst

--- src/Aufgabe3_16.java
+++ src/Aufgabe3_16.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class Aufgabe3_16 {
+    public static void main(String[] args) {
+        //Codeausschnitt aus Buch
+        System.out.println("Lösung mit while loop aus Buch");
+        int i = 20;
+        while (i > 0) {
+            System.out.println(i);
+            i -= 2;
+        }
+        System.out.println();
+        System.out.println("Meine Lösung mit for loop");
+        //meine Lösung mit for loop
+        for (int ii=20; ii > 0; ii -=2){
+            System.out.println(ii);
+        }
+
+    }
 }
